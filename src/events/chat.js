4aa3387f951ea2140091952f91adc0e544312c03
Miqhtiedev <miqhtiedev@gmail.com>
2021-04-29 16:47:47 -0400

    js is dumb

--- src/events/chat.js
+++ src/events/chat.js
@@ -1,7 +1,6 @@
-const Discord,
-  { Util } = require('discord.js');
+const { Util, WebhookClient } = require('discord.js');
 
-const guildChatLog = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
+const guildChatLog = new WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
 
 module.exports = function chat(bot, username, message, translate, jsonMsg, matches) {
   if (jsonMsg.extra === undefined) return;
@@ -9,11 +8,11 @@ module.exports = function chat(bot, username, message, translate, jsonMsg, match
 
   const name = getNameFromMessage(message.split(':').shift());
   sendEmbed({
-    description: Util.cleanContent(message.split(':')[1]),
+    description: new Util.cleanContent(message.split(':')[1]),
     color: '#12a602',
     timestamp: new Date(),
     author: {
-      name: Util.cleanContent(message.split(':').shift()),
+      name: new Util.cleanContent(message.split(':').shift()),
       icon_url: 'https://mc-heads.net/avatar/' + name,
     },
   });
