f529c3f2f9f8971f39627894518780c179371072
Miqhtiedev <miqhtiedev@gmail.com>
2021-04-30 10:24:23 -0400

    make embed color match rank color

--- src/events/chat.js
+++ src/events/chat.js
@@ -6,14 +6,14 @@ module.exports = function chat(bot, username, message, translate, jsonMsg, match
   if (jsonMsg.extra === undefined) return;
   if (!jsonMsg.extra[0].text.startsWith('§2Guild > ')) return;
 
-  const name = getNameFromMessage(message.split(':').shift());
+  const data = getPlayerDataFromMessage(message.split(':').shift());
   sendEmbed({
     description: Util.escapeMarkdown(message.split(':')[1]),
-    color: '#12a602',
+    color: data.color,
     timestamp: new Date(),
     author: {
       name: Util.escapeMarkdown(message.split(':').shift()),
-      icon_url: 'https://mc-heads.net/avatar/' + name,
+      icon_url: 'https://mc-heads.net/avatar/' + data.name,
     },
   });
 };
@@ -22,8 +22,18 @@ function sendEmbed(embed) {
   guildChatLog.send({ embeds: [embed] }).catch(console.error);
 }
 
-function getNameFromMessage(msg) {
+function getPlayerDataFromMessage(msg) {
   const args = msg.split(' ');
-  if (args[0].startsWith('[')) return args[1];
-  return args[0];
+  if (args[0].startsWith('[')) {
+    let color = '';
+    if (args[0].startsWith('[VIP')) {
+      color = '#27f238';
+    } else if (args[0].startsWith("[MVP++")){
+      color = "#f5c311"
+    } else {
+       color = '#4fe2ff';
+    }
+    return { name: args[1], color: color };
+  }
+  return { name: args[0], color: '12a602' };
 }
